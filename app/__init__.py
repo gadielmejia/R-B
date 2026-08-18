1b70a5759e1b29af12a2b439b8a4a1949c582fa7
tatanvargas2601-sketch <tatanvargas2601@gmail.com>
2026-08-18 14:44:36 -0500

    Servidores

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app.config.from_object(Config)
     init_cloudinary(app)
     db.init_app(app)
-    CORS(app, origins=["http://localhost:5173"])
+    CORS(app, origins=["http://localhost:5173", "http://localhost:8080", "http://localhost:65372/"])
     migrate = Migrate(app, db)
 
     from app.models import (
